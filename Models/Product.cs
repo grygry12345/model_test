fde80471058691e3225b865e6748077886d3ebaf
grygry12345 <41829095+grygry12345@users.noreply.github.com>
2021-05-17 14:27:54 +0300

    Fix some models

--- Models/Product.cs
+++ Models/Product.cs
@@ -6,14 +6,9 @@ namespace model_test.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
-<<<<<<< HEAD
-        public string Type { get; set; }
-        public ICollection<Order> Orders { get; set; }
-=======
         public bool IsSalable {get; set; }
         public OrderItem OrderItem { get; set; }
         public Operation ProductType {get; set; }
         public ICollection<SubProduct> SubProduct { get; set; }
->>>>>>> parent of 354d828 (Simpler Data Model)
     }
 }
